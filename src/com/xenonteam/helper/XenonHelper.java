a32081fed1518481c61d7ece940c778f8fde62b1
tim4242 <tim4242@gmx.de>
2015-05-01 19:08:48 +0200

    update

--- src/com/xenonteam/helper/XenonHelper.java
+++ src/com/xenonteam/helper/XenonHelper.java
@@ -10,40 +10,38 @@ public class XenonHelper
 
 	public static void main(String[] args)
 	{
-		if(!(args.length >= 1))
+		if (!(args.length >= 1))
 		{
 			System.out.println("No arguments given!");
 			return;
 		}
-		
+
 		String helper = args[0];
-		
+
 		ArrayList<String> toGiveArgs = new ArrayList<String>(Arrays.asList(args));
 		toGiveArgs.remove(0);
-		
+
 		String error = "none";
-		
-		if(helper.equalsIgnoreCase("sprite") || helper.equalsIgnoreCase("spritesheet"))
+
+		if (helper.equalsIgnoreCase("sprite") || helper.equalsIgnoreCase("spritesheet"))
 		{
 			error = SpriteSheetHelper.run(toGiveArgs);
-		}
-		else
+		} else
 		{
 			System.out.println("No helpers with the name \"" + helper + "\" found!");
 			return;
 		}
-		
-		if(!error.equalsIgnoreCase("none"))
+
+		if (!error.equalsIgnoreCase("none"))
 		{
 			System.out.println("The helper " + helper + " failed to execute!");
 			System.out.println("Error: " + error);
 			return;
-		}
-		else
+		} else
 		{
 			System.out.println("The helper " + helper + " finished executing!");
 		}
-		
+
 	}
 
 }
