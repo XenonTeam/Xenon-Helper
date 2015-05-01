7a93c770deb1cbaef4665ff74b84f9761d2d7e66
tim4242 <tim4242@gmx.de>
2015-05-01 18:08:51 +0200

    update

--- src/com/xenonteam/helper/XenonHelper.java
+++ src/com/xenonteam/helper/XenonHelper.java
@@ -1,11 +1,49 @@
 package com.xenonteam.helper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.xenonteam.helper.spritesheet.SpriteSheetHelper;
+
 public class XenonHelper
 {
 
 	public static void main(String[] args)
 	{
-
+		if(!(args.length >= 1))
+		{
+			System.out.println("No arguments given!");
+			return;
+		}
+		
+		String helper = args[0];
+		
+		ArrayList<String> toGiveArgs = new ArrayList<String>(Arrays.asList(args));
+		toGiveArgs.remove(0);
+		
+		String error = "none";
+		
+		if(helper.equalsIgnoreCase("sprite") || helper.equalsIgnoreCase("spritesheet"))
+		{
+			error = SpriteSheetHelper.run(toGiveArgs);
+		}
+		else
+		{
+			System.out.println("No helpers with the name \"" + helper + "\" found!");
+			return;
+		}
+		
+		if(!error.equalsIgnoreCase("none"))
+		{
+			System.out.println("The helper " + helper + " failed to execute!");
+			System.out.println("Error: " + error);
+			return;
+		}
+		else
+		{
+			System.out.println("The helper " + helper + " finished executing!");
+		}
+		
 	}
 
 }
---
+++ src/com/xenonteam/helper/spritesheet/SpriteSheetHelper.java
@@ -0,0 +1,13 @@
+package com.xenonteam.helper.spritesheet;
+
+import java.util.ArrayList;
+
+public class SpriteSheetHelper
+{
+
+	public static String run(ArrayList<String> args)
+	{
+		return "none";
+	}
+	
+}
